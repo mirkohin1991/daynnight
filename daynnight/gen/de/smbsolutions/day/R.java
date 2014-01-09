6c62659cb46ef7cc4ded45ab4cd8de32019ddc36
siku92 <kussmann.simon@googlemail.com>
2014-01-09 14:28:41 +0100

    Sektion gelöscht, Titel angepasst

--- daynnight/gen/de/smbsolutions/day/R.java
+++ daynnight/gen/de/smbsolutions/day/R.java
@@ -40,7 +40,6 @@ public final class R {
         public static final int app_name=0x7f050000;
         public static final int title_section1=0x7f050002;
         public static final int title_section2=0x7f050003;
-        public static final int title_section3=0x7f050004;
     }
     public static final class style {
         /** 
